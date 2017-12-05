0c5ed52ce25d14510ab9c109d12907798907cd4a
Kirk Ke Tang <kirkketang@126.com>
2017-12-05 21:06:50 +0800

    stop using magic numbers

--- src/KonvaApp.js
+++ src/KonvaApp.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { Stage } from 'react-konva';
-import { IMAGE_WIDTH, IMAGE_HEIGHT } from './common/Config';
+import { IMAGE_WIDTH, IMAGE_HEIGHT, LAYERS, STAGES } from './common/Config';
 import ImageLayer from './ImageLayer';
 
-const ImageSources = [...new Array(9).keys()].map(key => require('./assets/400x200-' + key + '.jpeg'))
+const ImageSources = [...new Array(LAYERS).keys()].map(key => require('./assets/400x200-' + key + '.jpeg'))
 
-const getNewLayers = () => [...new Array(9).keys()];
-const getNewImageData = () => [...new Array(100).keys()].map(stageKey => [...new Array(9).keys()].map(imageKey => ({
+const getNewLayers = () => [...new Array(LAYERS).keys()];
+const getNewImageData = () => [...new Array(STAGES).keys()].map(stageKey => [...new Array(LAYERS).keys()].map(imageKey => ({
   opacity: Math.random(),
   rotation: 360 * Math.random(),
   x: IMAGE_WIDTH * (.2 * Math.random() - .1),
@@ -45,7 +45,7 @@ export default class KonvaApp extends Component {
           <h1>change one layer</h1>
         </button>
         </div>
-        {[...new Array(100).keys()].map(stageKey => (
+        {[...new Array(STAGES).keys()].map(stageKey => (
           <Stage style={{display: 'inline-block'}} key={stageKey} width={IMAGE_WIDTH} height={IMAGE_HEIGHT}>
             {this.state.layers.map(imageKey => (
               <ImageLayer
--- src/common/Config.js
+++ src/common/Config.js
@@ -1,2 +1,4 @@
 export const IMAGE_WIDTH = 400
 export const IMAGE_HEIGHT = 200
+export const LAYERS = 9
+export const STAGES = 100
